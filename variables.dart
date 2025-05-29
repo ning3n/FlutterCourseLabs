f2d010fa213bbb7ec44fccd6844c37ad9827c81b
Jesus Miguel Rodriguez <52975938+ning3n@users.noreply.github.com>
2025-05-29 18:23:56 -0400

    Update variables.dart

--- variables.dart
+++ variables.dart
@@ -1,3 +1,14 @@
+class Person{
+    String name;
+    int age;
+
+    Person(this.name, this.age);
+
+    void displayInfo(){
+        print('Name: $name, Age: $age');
+    }
+}
+
 void main(){
     int age = 25;
     double height = 5.9;
@@ -10,7 +21,18 @@ void main(){
         'Charlie': 95
     };
     String? nullableString = null;
+    var city = 'New York'; //Type infered as string
+    String country = 'USA'; //Type annotation
+    const double pi = 3.14;
+    final DateTime currentTime = DateTime.now();
 
+    void greet(String name){
+        print('HELLO, $name!');
+    }
+
+
+    Person bob = Person('Bob', 30);
+    bob.displayInfo();
     print("Name: $name");
     print("Age: $age");
     print("Height: $height");
@@ -18,5 +40,10 @@ void main(){
     print('Fruits: $fruits');
     print('Score of Alice: ${scores['Alice']}');
     print('Nullable String: nullableString');
+    print('City: $city');
+    print('Country: $country');
+    print('Pi: $pi');
+    print('Current Time: $currentTime');
+    greet('Alice');
 
 }
