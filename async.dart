4caa8756afb435a666dce4ba7505b50b3e22828b
Jesus Miguel Rodriguez <52975938+ning3n@users.noreply.github.com>
2025-05-29 19:13:47 -0400

    Create async.dart

---
+++ async.dart
@@ -0,0 +1,11 @@
+import 'dart:async';
+
+void main() async {
+    Future<String> fetchData() async {
+        await Future.delayed(Duration(seconds: 2));
+        return 'Data fetched!';
+    }
+
+    String data = await fetchData();
+    print(data);
+}
